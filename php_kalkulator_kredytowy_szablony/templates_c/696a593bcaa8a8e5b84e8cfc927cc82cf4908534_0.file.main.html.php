4a08e3806f1cbf48cfebc8a20db4e89e3413908b
michalstas <56248540+michalstas@users.noreply.github.com>
2022-03-22 06:13:03 +0100

    Add files via upload

---
+++ php_kalkulator_kredytowy_szablony/templates_c/696a593bcaa8a8e5b84e8cfc927cc82cf4908534_0.file.main.html.php
@@ -0,0 +1,157 @@
+<?php
+/* Smarty version 4.1.0, created on 2022-03-22 05:35:55
+  from 'C:\xampp\htdocs\php_kalkulator_kredytowy_szablony\templates\main.html' */
+
+/* @var Smarty_Internal_Template $_smarty_tpl */
+if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
+  'version' => '4.1.0',
+  'unifunc' => 'content_6239522b088db5_37843215',
+  'has_nocache_code' => false,
+  'file_dependency' => 
+  array (
+    '696a593bcaa8a8e5b84e8cfc927cc82cf4908534' => 
+    array (
+      0 => 'C:\\xampp\\htdocs\\php_kalkulator_kredytowy_szablony\\templates\\main.html',
+      1 => 1647923752,
+      2 => 'file',
+    ),
+  ),
+  'includes' => 
+  array (
+  ),
+),false)) {
+function content_6239522b088db5_37843215 (Smarty_Internal_Template $_smarty_tpl) {
+$_smarty_tpl->_loadInheritance();
+$_smarty_tpl->inheritance->init($_smarty_tpl, false);
+?>
+<!doctype html>
+<html lang="pl">
+<head>
+    <meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta name="description" content="<?php echo (($tmp = $_smarty_tpl->tpl_vars['page_description']->value ?? null)===null||$tmp==='' ? "Opis domyślny" ?? null : $tmp);?>
+">
+
+    <title><?php echo (($tmp = $_smarty_tpl->tpl_vars['page_title']->value ?? null)===null||$tmp==='' ? "Tytuł domyślny" ?? null : $tmp);?>
+</title>
+
+	<link rel="stylesheet" href="http://yui.yahooapis.com/pure/0.4.2/pure.css">
+
+    <!--[if lte IE 8]>
+        <link rel="stylesheet" href="<?php echo $_smarty_tpl->tpl_vars['app_url']->value;?>
+/css/main-grid-old-ie.css">
+    <![endif]-->
+    <!--[if gt IE 8]><!-->
+        <link rel="stylesheet" href="<?php echo $_smarty_tpl->tpl_vars['app_url']->value;?>
+/css/main-grid.css">
+    <!--<![endif]-->
+  
+    <!--[if lte IE 8]>
+        <link rel="stylesheet" href="<?php echo $_smarty_tpl->tpl_vars['app_url']->value;?>
+/css/layouts/marketing-old-ie.css">
+    <![endif]-->
+    <!--[if gt IE 8]><!-->
+        <link rel="stylesheet" href="<?php echo $_smarty_tpl->tpl_vars['app_url']->value;?>
+/css/layouts/marketing.css">
+    <!--<![endif]-->
+
+    <link rel="stylesheet" href="<?php echo $_smarty_tpl->tpl_vars['app_url']->value;?>
+/css/style.css">
+<?php if ($_smarty_tpl->tpl_vars['hide_intro']->value) {?>
+    <link rel="stylesheet" href="<?php echo $_smarty_tpl->tpl_vars['app_url']->value;?>
+/css/style_hide_intro.css">
+<?php }?>
+	
+	<link rel="stylesheet" href="http://netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css">
+
+	<?php echo '<script'; ?>
+ src="<?php echo $_smarty_tpl->tpl_vars['app_url']->value;?>
+/js/jquery.min.js"><?php echo '</script'; ?>
+>
+	<?php echo '<script'; ?>
+ src="<?php echo $_smarty_tpl->tpl_vars['app_url']->value;?>
+/js/softscroll.js"><?php echo '</script'; ?>
+>
+
+</head>
+<body>
+
+<div id="app_top" class="header">
+    <div class="home-menu pure-menu pure-menu-open pure-menu-horizontal pure-menu-fixed">
+        <a class="pure-menu-heading" href="">Kalkulator Kredytowy</a>
+        <ul>
+            <li class="pure-menu-selected"><a href="#app_top">Góra strony</a></li>
+            <li><a href="#app_content">Kalkulator kredytowy</a></li>
+        </ul>
+    </div>
+</div>
+
+<div class="splash-container">
+    <div class="splash">
+        <h1 class="splash-head">Kalkulator</h1>
+        <p class="splash-subhead">
+             Kalkulator służący do obliczania kredytu
+        </p>
+        <p>
+            <a href="#app_content" class="pure-button pure-button-primary">Idź do kalkulatora</a>
+        </p>
+    </div>
+</div>
+
+<div class="content-wrapper">
+
+    <div id="app_content" class="content">
+
+<?php 
+$_smarty_tpl->inheritance->instanceBlock($_smarty_tpl, 'Block_19730440566239522b086ce9_09287767', 'content');
+?>
+
+
+    </div>
+
+    <div class="footer l-box is-center">
+		<p>
+<?php 
+$_smarty_tpl->inheritance->instanceBlock($_smarty_tpl, 'Block_11631853056239522b088001_91101177', 'footer');
+?>
+
+		</p>
+        <p>Widok oparty na stylach i szablonie <a href="http://purecss.io/" target="_blank">Pure CSS Yahoo!</a>. </p>
+    </div>
+
+</div>
+
+
+</body>
+</html><?php }
+/* {block 'content'} */
+class Block_19730440566239522b086ce9_09287767 extends Smarty_Internal_Block
+{
+public $subBlocks = array (
+  'content' => 
+  array (
+    0 => 'Block_19730440566239522b086ce9_09287767',
+  ),
+);
+public function callBlock(Smarty_Internal_Template $_smarty_tpl) {
+?>
+ Domyślna treść zawartości .... <?php
+}
+}
+/* {/block 'content'} */
+/* {block 'footer'} */
+class Block_11631853056239522b088001_91101177 extends Smarty_Internal_Block
+{
+public $subBlocks = array (
+  'footer' => 
+  array (
+    0 => 'Block_11631853056239522b088001_91101177',
+  ),
+);
+public function callBlock(Smarty_Internal_Template $_smarty_tpl) {
+?>
+ Domyślna treść stopki .... <?php
+}
+}
+/* {/block 'footer'} */
+}
